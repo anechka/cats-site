96770fbda2735ac8bf12d8a95be50ca1d5b8a03b
?????? <??????@??????-??>
2013-01-06 23:02:30 +0400

    UserInterface updated to JS code. Russian lang. locale added in menangen.js module Plates.js integrated for binding templates ( rendering table of cats )

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 class Cat(models.Model):
     name = models.CharField(max_length=16)
-    age = models.DecimalField(decimal_places=1, max_digits=3)
+    age = models.DecimalField(decimal_places=0, max_digits=2)
     bill_in_day = models.DecimalField(decimal_places=1, max_digits=4)
 
 
